eed7ce7ed394338a983de8c222f1a65058ef3437
Nyuyseni Patrice <patricenyuyseni>
2026-04-14 14:47:39 +0100

    search input buttons

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,18 +3,31 @@ import type { ChangeEvent } from "react";
 interface Props {
   query: string;
   setQuery: (value: string) => void;
+  onSearch?: () => void;
 }
 
-export default function SearchBar({ query, setQuery }: Props) {
+export default function SearchBar({ query, setQuery, onSearch }: Props) {
   return (
-    <input
-      type="text"
-      placeholder="Search movies..."
-      value={query}
-      onChange={(e: ChangeEvent<HTMLInputElement>) =>
-        setQuery(e.target.value)
-      }
-      className="bg-[#111827] px-5 py-3 rounded-lg w-[320px] outline-none border border-gray-700 focus:border-red-500 transition text-white"
-    />
+    <div className="flex items-center bg-[#111827] rounded-lg border border-gray-700 focus-within:border-red-500 transition overflow-hidden w-[320px]">
+      
+      
+      <input
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setQuery(e.target.value)
+        }
+        className="bg-transparent px-4 py-3 outline-none text-white w-full"
+      />
+
+      
+      <button
+        onClick={onSearch}
+        className="px-4 py-3 bg-red-600 hover:bg-red-700 transition text-white"
+      >
+        Search
+      </button>
+    </div>
   );
 }
