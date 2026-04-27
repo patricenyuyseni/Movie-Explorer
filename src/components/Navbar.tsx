319a60940bc2210411adda4e97d1b9f5d326a5b9
Nyuyseni Patrice <patricenyuyseni>
2026-04-27 05:21:10 +0100

    changes made on the herosectiom.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,12 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-10 py-6 bg-[#0b0f19] text-white">
       
-      {/* Title */}
+      
       <Link to="/" className="text-2xl font-bold">
         🎬 Movie Explorer
       </Link>
 
-      {/* Links */}
+      
       <div className="flex gap-6 text-gray-300">
         <Link to="/" className="hover:text-white transition">
           Home
