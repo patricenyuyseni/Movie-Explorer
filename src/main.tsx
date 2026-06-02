a353b891238f8166ff9fa08b550dd937299009c7
Patrice Nyuyseni <patriceflezy123@gmail.com>
2026-06-02 10:13:19 +0100

    my watchlist breaks because i did not wrap it in my main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { WatchlistProvider } from "./context/watchlistcontext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <WatchlistProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </WatchlistProvider>
   </React.StrictMode>
 );
